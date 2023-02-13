4b2dbbb42e2bb134afca9a591024bf4f40791e32
Matheus <matheuspcouto70@gmail.com>
2023-02-13 11:31:36 -0300

    renomeando modules para components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,23 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { SiteAtivoGuard } from './guards/site-ativo.guard';
 
-import { AguardeComponent } from './modules/aguarde/aguarde.component';
+import { AguardeComponent } from './components/aguarde/aguarde.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FooterComponent } from './modules/footer/footer.component';
-import { HomeComponent } from './modules/home/home.component';
-import { SucessoComprovanteComponent } from './modules/sucesso-comprovante/sucesso-comprovante.component';
-import { LoadderComponent } from './modules/loadder/loadder.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { SucessoComprovanteComponent } from './components/sucesso-comprovante/sucesso-comprovante.component';
+import { LoadderComponent } from './components/loadder/loadder.component';
 import { FormsModule, ReactiveFormsModule  } from '@angular/forms';
 import { NgxMaskModule } from "ngx-mask";
-import { ProdutosComponent } from './modules/produtos/produtos.component';
-import { DetalhesProdutoComponent } from './modules/produtos/detalhes-produto/detalhes-produto.component';
+import { ProdutosComponent } from './components/produtos/produtos.component';
+import { DetalhesProdutoComponent } from './components/produtos/detalhes-produto/detalhes-produto.component';
 import { HttpClientModule } from '@angular/common/http';
-import { CarrinhoComponent } from './modules/carrinho/carrinho.component';
-import { FinalizarPedidoComponent } from './modules/finalizar-pedido/finalizar-pedido.component';
+import { CarrinhoComponent } from './components/carrinho/carrinho.component';
+import { FinalizarPedidoComponent } from './components/finalizar-pedido/finalizar-pedido.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToastrModule } from 'ngx-toastr';
-import { PagamentoComponent } from './modules/pagamento/pagamento.component';
+import { PagamentoComponent } from './components/pagamento/pagamento.component';
 
 @NgModule({
   declarations: [
