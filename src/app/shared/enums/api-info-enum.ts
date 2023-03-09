5d472a387b9f009fe2574af68060568297f0fb27
Matheus <matheuspcouto70@gmail.com>
2023-03-09 13:54:29 -0300

    Ajustes api info

--- src/app/shared/enums/api-info-enum.ts
+++ src/app/shared/enums/api-info-enum.ts
@@ -1,8 +1,3 @@
 export enum ApiInfo {
-  // TODO: Sempre trocar o host antes de subir pra master
-
-  // PLANILHA PRODUÇÃO
-
-  // PLANILHA DE TESTES
   HOST = 'https://script.google.com/macros/s/AKfycbxl-zHPHabalTdWwzkjWHRsvLlPGYCLUitUNABXDIYRMA7bpWdaV8Bwi6nQ_l25rWf2nQ/exec'
 }
